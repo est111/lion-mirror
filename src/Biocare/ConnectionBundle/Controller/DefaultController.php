ee2f00e2debac33978ac0e75f012d23e451f3fe3
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-25 14:36:00 +0200

    Request handler for Connections

--- src/Biocare/ConnectionBundle/Controller/DefaultController.php
+++ src/Biocare/ConnectionBundle/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace Biocare\ConnectionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template; 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use Symfony\Component\HttpFoundation\Request;
