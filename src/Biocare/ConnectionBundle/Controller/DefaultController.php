977743fc93e54b67ef2a793d7429cac30dfbc172
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-25 14:35:44 +0200

    Entity Connextion

--- src/Biocare/ConnectionBundle/Controller/DefaultController.php
+++ src/Biocare/ConnectionBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Biocare\ConnectionBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
