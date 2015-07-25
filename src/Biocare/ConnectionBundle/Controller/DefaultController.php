49bac0e69def1ad0c70a7bcb4e2576a5ac8d0b32
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-25 18:27:42 +0200

    Yellowknife Bower Bundle

--- src/Biocare/ConnectionBundle/Controller/DefaultController.php
+++ src/Biocare/ConnectionBundle/Controller/DefaultController.php
@@ -59,6 +59,7 @@ class DefaultController extends Controller {
 
             // data is an array with "name", "email", and "message" keys
             $data = $form->getData();
+            return $this->redirect($this->generateUrl('connexion_new', array('source'=>$source,'destination'=>$destination) ));
         }
         return array(
             'form' => $form->createView(),
