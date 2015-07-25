0695f5cc166c52baf5430433782573b56fc5108b
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-25 14:34:19 +0200

    Entity Connextion

--- src/Biocare/ConnectionBundle/Entity/Connexion.php
+++ src/Biocare/ConnectionBundle/Entity/Connexion.php
@@ -13,10 +13,18 @@ use Doctrine\ORM\Mapping as ORM;
 class Connexion
 {
     
-    public function __construct() {
+    
+    
+    public function __construct(String $source_string, String $destination_string) {
+        
         
         $createdAt =  new \DateTime('NOW', \DateTimeZone::UTC );
+        $source      = preg_replace('/\s+/', '', $source_string);
+        $destination = preg_replace('/\s+/', '', $destination_strings);
+        
         $this->setCreatedAt($createdAt);
+        $this->setSource($source);
+        $this->setDestination($destination);
         
         return $this;
     }
@@ -44,7 +52,7 @@ class Connexion
     }
     
     /**
-     * @var \DateTime DataTime of Connextion [DATETIME WITH TIME ZONE]
+     * @var \DateTime DataTime when Connextion were created Datatime with UTC  
      * 
      * @ORM\Column(name="createdAt", type="datetimetz")
      * 
@@ -57,6 +65,41 @@ class Connexion
 
     public function setCreatedAt(\DateTime $createdAt) {
         $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @var String Source of connextion whitespace outed by constructor
+     * 
+     * @ORM\Column(name="source", type="string", length=255)
+     * 
+     */
+    private $source;
+
+    public function getSource() {
+        return $this->source;
+    }
+
+    public function setSource(String $source) {
+        $this->source = $source;
+        return $this;
+    }
+
+    
+    /**
+     * @var String Destination of connextion whitespace outed by constructor 
+     * 
+     * @ORM\Column(name="destination", type="string", length=255)
+     * 
+     */
+    private $destination;
+ 
+    public function getDestination() {
+        return $this->destination;
+    }
+
+    public function setDestination(String $destination) {
+        $this->destination = $destination;
         return $this;
     }
 
