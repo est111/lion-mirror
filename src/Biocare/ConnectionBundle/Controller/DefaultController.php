10d77c64454e1033d6ddc38334a612f8b8be1a6a
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-25 18:40:05 +0200

    Yellowknife Bower Bundle

--- src/Biocare/ConnectionBundle/Controller/DefaultController.php
+++ src/Biocare/ConnectionBundle/Controller/DefaultController.php
@@ -74,7 +74,7 @@ class DefaultController extends Controller {
      *
      * @Route("/connexion/{source}/{destination}/", name="connexion_new")
      * @Method("GET")
-     * @Template()
+     * @Template("BiocareConnectionBundle:Default:new.html.twig")
      */
     public function newAction($source, $destination)
     {
