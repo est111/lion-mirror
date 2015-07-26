67040759c2376dc706ac7daab25e3dbad0b0eece
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-26 19:09:29 +0200

    Yellowknife Bower Bundle

--- src/Biocare/CarrierBundle/Controller/DefaultController.php
+++ src/Biocare/CarrierBundle/Controller/DefaultController.php
@@ -20,8 +20,10 @@ class DefaultController extends Controller
         $resp = $httpapi->tariff('191001');
         $resp = mb_convert_encoding($resp, "utf-8", "windows-1251");
         $response = json_decode($resp);
-        $resp = $response->delivery_ways;
+        dump($response);
         
-        return array('resp'=>  $resp);
+        //$resp = $response->delivery_ways;
+        exit;
+        return array('resp'=>  $response);
     }
 }
