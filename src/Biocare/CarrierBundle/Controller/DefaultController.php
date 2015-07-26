295e4b66d07363b72c8ea6946c3d32f29ba98112
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-26 18:21:04 +0200

    Yellowknife Bower Bundle

--- src/Biocare/CarrierBundle/Controller/DefaultController.php
+++ src/Biocare/CarrierBundle/Controller/DefaultController.php
@@ -22,7 +22,7 @@ class DefaultController extends Controller
         $response->setContent($resp);
         $response->setStatusCode(Response::HTTP_OK);
         $response->headers->set('Content-Type', 'text/json');
-        $response-setCharset('Windows-1251');
+        $response->setCharset('Windows-1251');
         // prints the HTTP headers followed by the content
         $response->send();
         return array('resp'=>  $response->send());
