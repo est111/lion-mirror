3567b5293fef2d72ac86ce22551ce7cff68d3f18
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-26 20:01:11 +0200

    Yellowknife Bower Bundle

--- src/Biocare/CarrierBundle/Controller/DefaultController.php
+++ src/Biocare/CarrierBundle/Controller/DefaultController.php
@@ -15,7 +15,7 @@ class DefaultController extends Controller
     public function indexAction($zip)
     {
         $httpapi = new \Biocare\CarrierBundle\Entity\HttpApi();
-        $resp_raw = $httpapi->tariff('191001');
+        $resp_raw = $httpapi->tariff($zip);
         $resp_conv = mb_convert_encoding($resp_raw, "utf-8", "windows-1251");
         $resp = json_decode($resp_conv);
         $html ="<select>";
