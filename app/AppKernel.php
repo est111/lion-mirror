954cda562d97a034b83ec64053615eabb1525a20
Karol Gontarski <karol.gontarski@gmail.com>
2015-07-26 09:00:27 -0400

    Carrier

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Biocare\ConnectionBundle\BiocareConnectionBundle(),
             new \Yellowknife\BowerBundle\YellowknifeBowerBundle(),
+            new Biocare\CarrierBundle\BiocareCarrierBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Biocare/CarrierBundle/BiocareCarrierBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Biocare\CarrierBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BiocareCarrierBundle extends Bundle
+{
+}
