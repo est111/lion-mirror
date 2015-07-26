36b1b443437f2e341d8f4711aed5cc66b44bc52f
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-26 18:20:13 +0200

    Yellowknife Bower Bundle

--- src/Biocare/CarrierBundle/Controller/DefaultController.php
+++ src/Biocare/CarrierBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Biocare\CarrierBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,6 +18,14 @@ class DefaultController extends Controller
         $httpapi = new \Biocare\CarrierBundle\Entity\HttpApi();
         $resp = $httpapi->info_zip();
         print_r($resp-setCharset('Windows-1251'));
-        return array('resp'=>  $resp);
+        
+        $response = new Response();
+        $response->setContent($resp);
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'text/json');
+
+        // prints the HTTP headers followed by the content
+        $response->send();
+        return array('resp'=>  $response->send());
     }
 }
