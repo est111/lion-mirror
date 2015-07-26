fa07d0bd575d0259255aa841231976febdcf8ae1
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-26 19:48:21 +0200

    Yellowknife Bower Bundle

--- src/Biocare/CarrierBundle/Controller/DefaultController.php
+++ src/Biocare/CarrierBundle/Controller/DefaultController.php
@@ -19,8 +19,11 @@ class DefaultController extends Controller
         $resp_conv = mb_convert_encoding($resp_raw, "utf-8", "windows-1251");
         $resp = json_decode($resp_conv);
         $html ="<select>";
-        foreach ($resp->delivery_ways as $dw){ dump($dw->Код);
-        $html +="<option value='".$dw->Стоимость."'>".$dw->Наименование." - ".$dw->Стоимость." - ".$dw->Код."</option>";
+        foreach ($resp->delivery_ways as $dw){ 
+            
+            dump($dw->Код);
+            $html +="<option value='".$dw->Стоимость."'>".$dw->Наименование." - ".$dw->Стоимость." - ".$dw->Код."</option>";
+            
         }
         $html +="</select>";
         
