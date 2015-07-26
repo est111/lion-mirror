5599ad9ee80755edfe0e11a9ae8786f902dc98c4
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-26 19:35:24 +0200

    Yellowknife Bower Bundle

--- src/Biocare/CarrierBundle/Controller/DefaultController.php
+++ src/Biocare/CarrierBundle/Controller/DefaultController.php
@@ -11,15 +11,20 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/httpapi")
+     * @Route("/httpapi/{zip}")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction($zip)
     {
         $httpapi = new \Biocare\CarrierBundle\Entity\HttpApi();
         $resp = $httpapi->tariff('191001');
         $resp = mb_convert_encoding($resp, "utf-8", "windows-1251");
         $response = json_decode($resp);
+        $response ="<select>";
+        foreach ($response->delivery_ways as $dw){
+        $response +="<option value='".$dw['Стоимость']."'>".$dw['Наименование']." - ".$dw['Стоимость']." - ".$dw['Код']."</option>";
+        }
+        $response +="</select>";
         return array('resp'=>  $response->delivery_ways);
     }
 }
