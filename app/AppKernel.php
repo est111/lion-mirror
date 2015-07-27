4dee754ff2fc5527021ec128747701e19e472c1c
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-27 13:37:56 +0200

    Call Register Entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Biocare\ConnectionBundle\BiocareConnectionBundle(),
-            new \Yellowknife\BowerBundle\YellowknifeBowerBundle(),
+            new Yellowknife\BowerBundle\YellowknifeBowerBundle(),
             //new Biocare\CarrierBundle\BiocareCarrierBundle(),
             new Biocare\PanelBundle\BiocarePanelBundle(),
             new Biocare\CallBundle\BiocareCallBundle(),
