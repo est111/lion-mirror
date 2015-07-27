4c8d95b88cb6aa995eabd76b0ff11b600a5f26e3
Karol Gontarski <karol.gontarski@gmail.com>
2015-07-27 01:51:15 -0400

    Server Push

---
+++ src/Biocare/CallBundle/Form/CallRegisterType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Biocare\CallBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CallRegisterType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('createdFromIp')
+            ->add('created')
+            ->add('createdBy')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Biocare\CallBundle\Entity\CallRegister'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'biocare_callbundle_callregister';
+    }
+}
