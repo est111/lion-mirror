ea868348af8c8a9467da5802ff6e4454aafbe6ae
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-27 09:09:52 +0200

    Call Register Entity

--- src/Biocare/PanelBundle/Controller/DefaultController.php
+++ src/Biocare/PanelBundle/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction($name)
     {
+        $this->get('callregister');
         return array('name' => $name);
     }
 }
