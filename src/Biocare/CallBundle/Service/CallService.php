874bb0a72057d506d7574a41f2d101b36b24e664
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-27 11:21:14 +0200

    Call Register Entity

--- src/Biocare/CallBundle/Service/CallService.php
+++ src/Biocare/CallBundle/Service/CallService.php
@@ -4,6 +4,7 @@ namespace Biocare\CallBundle\Service;
 
 use Doctrine\ORM\EntityManager;
 use Biocare\CallBundle\Entity\CallRegister;
+        use Symfony\Component\Security\Core\Util\SecureRandom;
 
 /**
  * Description of CallRegister
@@ -24,17 +25,11 @@ class CallService {
     }
 
     public function __construct(EntityManager $entityManager, $user, $ip) {
-        $this->em = $entityManager;
-        if (isset($this->id)) {
-            $register = $this->em->getRepository('BiocareCallBundle:CallRegister')->findById($this->id);
-        } else {
-            $register = new CallRegister($user, $ip);
-            $this->em->persist($register);
-            $this->em->flush();
-            $this->setId($register->getId());
-        }
 
-        return $register;
+        $generator = new SecureRandom();
+        $this->setId($generator->nextBytes(10));
+        
+        
     }
 
 }
