80efbb0c3c69a63347b77f5be64f6e42b04da547
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-27 15:16:24 +0200

    Call Register Entity

--- src/Biocare/PanelBundle/Controller/DefaultController.php
+++ src/Biocare/PanelBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Biocare\CallBundle\Entity\CallRegister;
+use Biocare\CallBundle\Entity\Source;
 
 class DefaultController extends Controller {
 
@@ -25,8 +26,23 @@ class DefaultController extends Controller {
         $callregister = new CallRegister($user, $ip);
 
         $em = $this->getDoctrine()->getManager();
+        
+        
+        
         $em->persist($callregister);
         $em->flush();
+        
+        
+        $source_get = preg_replace('/\s+/', '', $request->get('source'));
+        
+        if($source_get){
+            $source = new Source();
+            $source->addCallregister($callregister);
+            $em->persist($source);
+        }
+        
+        
+        
 
         $session->set('callregister', $callregister);
 
