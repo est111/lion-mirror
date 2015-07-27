c710cae01ae9771b2976f9bdfc069ffc6d36b0fc
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-27 13:43:07 +0200

    Call Register Entity

---
+++ src/Biocare/PanelBundle/Controller/PanelController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Biocare\PanelBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class PanelController extends Controller {
+
+    /**
+     * @Route("/panel/", name="panel")
+     * @Template()
+     */
+    public function indexAction() {
+
+        $callregister = $this->get('session')->get('callregister');
+        
+        return array(
+            'callregister' => $callregister
+                );
+    }
+
+}
