b3e73aa4d86e38f0ffa7ad034e1bd06c426f4be0
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-27 12:46:04 +0200

    Call Register Entity

--- src/Biocare/PanelBundle/Controller/DefaultController.php
+++ src/Biocare/PanelBundle/Controller/DefaultController.php
@@ -5,37 +5,38 @@ namespace Biocare\PanelBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
 use Biocare\CallBundle\Entity\CallRegister;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction($name)
-    {
-        $user = $this->get('security.token_storage')->getToken()->getUser();
-        
-        $ip = $this->get('request')->getClientIp();
-    
+    public function indexAction($name) {
+
+
         $session = $this->get('session');
-        if(!isset($session->get('callregister')))
-        {   
-        $callregister = new CallRegister($user,$ip);
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($callregister);
-        $em->flush();
+        $callregister = $session->get('callregister');
         
-        $session = $this->get('session');
-        $session->set('callregister', $callregister->getId());
-    }
-        
-        $name = $callregister->getCreatedBy();   
-        
-        dump($callregister);
-        
+        if (!$callregister) {
+            
+            $user = $this->get('security.token_storage')->getToken()->getUser();
+            
+            $ip = $this->get('request')->getClientIp();
+            
+            $callregister = new CallRegister($user, $ip);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($callregister);
+            $em->flush();
+
+            $session->set('callregister',$callregister);
+        } 
+
+        $name = dump($callregister);
+
         return array('name' => $name);
     }
+
 }
