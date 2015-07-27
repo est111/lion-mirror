f79f595219e7ebf6bcbd50df5b7a3f3d0b86f66e
Karol Gontarski <karol.gontarski@gmail.com>
2015-07-27 01:26:16 -0400

    Server Push

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new \Yellowknife\BowerBundle\YellowknifeBowerBundle(),
             new Biocare\CarrierBundle\BiocareCarrierBundle(),
             new Biocare\PanelBundle\BiocarePanelBundle(),
+            new Biocare\CallBundle\BiocareCallBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Biocare/CallBundle/BiocareCallBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Biocare\CallBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BiocareCallBundle extends Bundle
+{
+}
