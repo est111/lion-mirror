e709f582a5ddcbb16a543618dc441e490bcdce5f
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-28 10:42:16 +0200

    Call Register Entity

--- src/Biocare/CustomerBundle/Form/CustomerType.php
+++ src/Biocare/CustomerBundle/Form/CustomerType.php
@@ -15,6 +15,7 @@ class CustomerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('callregister')
             ->add('firstname')
             ->add('lastname')
             ->add('fathername')
