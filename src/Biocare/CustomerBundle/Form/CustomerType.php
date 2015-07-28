0862472caaff979b3e5de617ce61f6cb024ea3aa
Karol Gontarski <karol.gontarski@gmail.com>
2015-07-28 03:57:06 -0400

    Server Push

---
+++ src/Biocare/CustomerBundle/Form/CustomerType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Biocare\CustomerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CustomerType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('fathername')
+            ->add('phonenumber')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Biocare\CustomerBundle\Entity\Customer'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'biocare_customerbundle_customer';
+    }
+}
