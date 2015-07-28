a936cae9c302f38e69a091b400771d5db3cfe807
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-28 15:27:46 +0200

    Call Register Entity

--- src/Biocare/AddressBundle/Form/AddressType.php
+++ src/Biocare/AddressBundle/Form/AddressType.php
@@ -15,7 +15,7 @@ class AddressType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('country')
+                ->add('country','country')
         ;
     }
     
