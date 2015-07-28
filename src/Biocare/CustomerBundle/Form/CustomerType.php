981628c6e034f21fb07439663e647c0e8f0aefd5
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-28 11:27:39 +0200

    Call Register Entity

--- src/Biocare/CustomerBundle/Form/CustomerType.php
+++ src/Biocare/CustomerBundle/Form/CustomerType.php
@@ -6,28 +6,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class CustomerType extends AbstractType
-{
+class CustomerType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('callregister','entity',array('class'=>'BiocareCallBundle:CallRegister'))
-            ->add('firstname')
-            ->add('lastname')
-            ->add('fathername')
-            ->add('phonenumber')
+                ->add('callregister', 'entity', array(
+                    'class' => 'BiocareCallBundle:CallRegister'
+                ))
+                ->add('firstname')
+                ->add('lastname')
+                ->add('fathername')
+                ->add('phonenumber')
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Biocare\CustomerBundle\Entity\Customer'
         ));
@@ -36,8 +36,8 @@ class CustomerType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'biocare_customerbundle_customer';
     }
+
 }
