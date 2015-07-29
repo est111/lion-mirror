ae3e8a3b7d8fdc47133b2517b33629638eb0013f
Karol Gontarski <karol.gontarski@gmail.com>
2015-07-29 14:12:48 -0400

    Server Push

---
+++ src/Biocare/OrderBundle/Entity/Orders.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Biocare\OrderBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Orders
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Biocare\OrderBundle\Entity\OrdersRepository")
+ */
+class Orders
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
