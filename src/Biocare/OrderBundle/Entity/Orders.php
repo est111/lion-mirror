daaaceef9f8157c97c9f5c5f6eae4d3147f4525c
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-29 20:06:25 +0200

    Call Register Entity

---
+++ src/Biocare/OrderBundle/Entity/Orders.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Biocare\OrderBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Orders
+ *
+ * @ORM\Table()
+ */
+class Orders
+{
+    
+        
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * @var \DateTime DataTime when Connextion were created Datatime with UTC  
+     * 
+     * @ORM\Column(name="createdAt", type="datetimetz")
+     * 
+     */
+    private $createdAt;
+    
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt) {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+  
+
+    
+}
