1be24e6658082c79b211b2131263cb7a55ece518
Karol Gontarski <karol.gontarski@gmail.com>
2015-07-29 14:18:21 -0400

    Server Push

---
+++ src/Biocare/OrderBundle/Form/OrdersType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Biocare\OrderBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OrdersType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('createdAt')
+            ->add('callregister')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Biocare\OrderBundle\Entity\Orders'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'biocare_orderbundle_orders';
+    }
+}
