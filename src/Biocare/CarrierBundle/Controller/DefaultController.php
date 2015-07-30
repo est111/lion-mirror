bf6d308ddcca4d8ebf000718d8a68c9c09bc261e
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-30 12:08:40 +0200

    Order Panel

--- src/Biocare/CarrierBundle/Controller/DefaultController.php
+++ src/Biocare/CarrierBundle/Controller/DefaultController.php
@@ -18,7 +18,7 @@ class DefaultController extends Controller
         $resp_raw = $httpapi->tariff($zip);
         $resp_conv = mb_convert_encoding($resp_raw, "utf-8", "windows-1251");
         $resp = json_decode($resp_conv);
-        $html ="<select class='select2'>";
+        $html ="<select class='select2' placeholder='chuj'>";
         foreach ($resp->delivery_ways as $dw){ 
             $html .="<option value='".$dw->Стоимость."'>".$dw->Наименование." - ".$dw->Стоимость." - ".$dw->Код."</option>";
         }
