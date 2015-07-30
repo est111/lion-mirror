f70ab847b380a819d95d24e2377912f51fff9ff2
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-07-30 14:17:12 +0200

    Order Panel

--- src/Biocare/StorageBundle/Form/StorageType.php
+++ src/Biocare/StorageBundle/Form/StorageType.php
@@ -6,24 +6,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class StorageType extends AbstractType
-{
+class StorageType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('name')
+        ->add('name')
+        ->add('item')
         ;
     }
 
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Biocare\StorageBundle\Entity\Storage'
         ));
@@ -32,8 +31,8 @@ class StorageType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'biocare_storagebundle_storage';
     }
+
 }
