648c26ef352ae685f281b4e483547b407c1f36b8
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-01 15:27:24 +0200

    Order Panel

--- src/Biocare/HttpApiBundle/Controller/DefaultController.php
+++ src/Biocare/HttpApiBundle/Controller/DefaultController.php
@@ -15,9 +15,9 @@ class DefaultController extends Controller {
      */
     public function indexAction($test = NULL) {
 
-        //$a = new \Biocare\HttpApiBundle\Entity\HttpApi('http://is.b2cpl.ru/portal/client_api.ashx','test','test');
+        $a = new \Biocare\HttpApiBundle\Entity\HttpApi('http://is.b2cpl.ru/portal/client_api.ashx','test','test');
         
-        $a = new \Biocare\HttpApiBundle\Entity\Curl();
+        //$a = new \Biocare\HttpApiBundle\Entity\Curl();
         $a->setUrl('http://is.b2cpl.ru/portal/client_api.ashx?client=test&key=test');
         
         $a->getQuery('&func=tarif&zip=125032&weight=1001&x=121&y=1&z=1&type=post_add&price=10000');
