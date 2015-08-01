73299ae62bba04df62023865d971d3273aa32c62
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-01 13:50:01 +0200

    Order Panel

--- src/Biocare/HttpApiBundle/Entity/HttpApi.php
+++ src/Biocare/HttpApiBundle/Entity/HttpApi.php
@@ -6,8 +6,46 @@ use Doctrine\ORM\Mapping as ORM;
 
 class HttpApi
 {
+   private $url;
+   private $username;
+   private $password;
+   private $response; 
    
-    
-    
-    
+   public function getUrl() {
+       return $this->url;
+   }
+
+   public function getUsername() {
+       return $this->username;
+   }
+
+   public function getPassword() {
+       return $this->password;
+   }
+
+   public function getResponse() {
+       return $this->response;
+   }
+
+   public function setUrl($url) {
+       $this->url = $url;
+       return $this;
+   }
+
+   public function setUsername($username) {
+       $this->username = $username;
+       return $this;
+   }
+
+   public function setPassword($password) {
+       $this->password = $password;
+       return $this;
+   }
+
+   public function setResponse($response) {
+       $this->response = $response;
+       return $this;
+   }
+
+   
 }
