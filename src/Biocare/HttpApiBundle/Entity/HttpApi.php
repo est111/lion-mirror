6721213ba86993396472abcf2365076a68d2353b
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-01 13:47:24 +0200

    Order Panel

--- src/Biocare/HttpApiBundle/Entity/HttpApi.php
+++ src/Biocare/HttpApiBundle/Entity/HttpApi.php
@@ -4,31 +4,10 @@ namespace Biocare\HttpApiBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * HttpApi
- *
- * @ORM\Table()
- * @ORM\Entity(repositoryClass="Biocare\HttpApiBundle\Entity\HttpApiRepository")
- */
 class HttpApi
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
-
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+   
+    
+    
+    
 }
