25adfbc5f60aa4c18bd5a3c4a2431b4e035d62e7
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-01 13:46:53 +0200

    Order Panel

--- src/Biocare/HttpApiBundle/Entity/RuB2CPL.php
+++ src/Biocare/HttpApiBundle/Entity/RuB2CPL.php
@@ -4,31 +4,14 @@ namespace Biocare\HttpApiBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * RuB2CPL
- *
- * @ORM\Table()
- * @ORM\Entity(repositoryClass="Biocare\HttpApiBundle\Entity\RuB2CPLRepository")
- */
+
 class RuB2CPL
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
 
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
+    public function Tarif(){
+        
     }
+    
+    
+    
 }
