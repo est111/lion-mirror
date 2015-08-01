7eef83c55b989d2135a067783b4191d67a9517e2
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-01 16:40:23 +0200

    Order Panel

--- src/Biocare/HttpApiBundle/Controller/DefaultController.php
+++ src/Biocare/HttpApiBundle/Controller/DefaultController.php
@@ -37,6 +37,11 @@ class DefaultController extends Controller {
             $b->tarif($arg[0],$arg[1],$arg[2],$arg[3],$arg[4]);
             $response .= $b->getResponseTEST();
             unset($b);
+            
+            $a = new \Biocare\HttpApiBundle\Entity\RuB2CPL(); 
+            $a->tarif($arg[0],$arg[1],$arg[2],'post',$arg[4]);
+            $response .= $a->getResponseTEST();
+            unset($a);
         }
         
           
