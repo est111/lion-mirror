6db96ce394485991c7976c67b374099672e0f6be
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-01 14:27:06 +0200

    Order Panel

--- src/Biocare/HttpApiBundle/Entity/HttpApi.php
+++ src/Biocare/HttpApiBundle/Entity/HttpApi.php
@@ -9,12 +9,12 @@ class HttpApi extends Curl
 {
    
     public function __construct($url,$username,$password) {     
-        $this->setUrl = $url."?client=".$username."&key=".$password;        
+        $this->setUrl = $url."?client=".$username."&key=".$password;  
+        parent::__construct();
     } 
     
     public function testApi(){
-       
-       return $this;
+       $this->get();
    }
 
     public function __toString() {
