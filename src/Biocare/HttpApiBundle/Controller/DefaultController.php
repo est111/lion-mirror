030b56c2a2d3b6e3f4e39028505a2f20a3d12765
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-01 17:30:45 +0200

    Order Panel

--- src/Biocare/HttpApiBundle/Controller/DefaultController.php
+++ src/Biocare/HttpApiBundle/Controller/DefaultController.php
@@ -9,66 +9,90 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller {
 
     /**
+     * @Route("/api/{country}/{company}/", name="api")
+     * @Template()
+     */
+    public function tarifAction($country, $company, $package) {
+
+        if ($country = 'ru') {
+            if ($company = 'b2cpl') {
+                $api = new \Biocare\HttpApiBundle\Entity\RuB2CPL();
+            }
+        }
+        
+        $api->tarif($zip, $weight, $dimensions, $type, $price);
+        $response = $api->getResponseHTML();
+        return array('name'=>$response);
+    }
+
+    /**
      * @Route("/api/", name="api")
      * @Template()
      */
     public function indexAction() {
 
-        $response = "";
-        
-        $args = array(
-            array('685000','1000',array('1','1','1'),'post_add','1000'),
-            array('664047','1000',array('1','1','1'),'post_add','1000'),
-            array('630078','1000',array('1','1','1'),'post_add','1000'),
-            array('121500','1000',array('1','1','1'),'post_add','1000'),
-            array('194156','1000',array('1','1','1'),'post_add','1000'),
-            array('678400','1000',array('1','1','1'),'post_add','1000'),
-        );
-        
-        
-//        $a = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');    
-//        $a->tarif();
-//        $response .= $a->getResponseTEST();
-//        
-        
-        
-        foreach ($args as $arg) {
-            $b = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test'); 
-            $b->tarif($arg[0],$arg[1],$arg[2],$arg[3],$arg[4]);
-            $response .= $b->getResponseTEST($arg[0])."<br/>";
-            unset($b);
-            
-            $a = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test'); 
-            $a->tarif($arg[0],$arg[1],$arg[2],$arg[3],$arg[4]*10);
-            $response .= $a->getResponseTEST($arg[0])."<br/>";
-            unset($a);
-            
-            $c = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test'); 
-            $c->tarif($arg[0],$arg[1],$arg[2],'post',$arg[4]);
-            $response .= $c->getResponseTEST($arg[0])."<br/>";
-            unset($c);
-            
-            $d = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test'); 
-            $d->tarif($arg[0],$arg[1],$arg[2],'post',$arg[4]*10);
-            $response .= $d->getResponseTEST($arg[0])."<br/>";
-            unset($d);
-            
-            $e = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test'); 
-            $e->tarif($arg[0],$arg[1],$arg[2],'+post',$arg[4]);
-            $response .= $e->getResponseTEST($arg[0])."<br/>";
-            unset($e);
-            
-            $f = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test'); 
-            $f->tarif($arg[0],$arg[1],$arg[2],'+post',$arg[4]*10);
-            $response .= $f->getResponseTEST($arg[0])."<br/>";
-            unset($f);           
-        }
-        
-          
-        
-        
-        
-        
+
+
+
+
+
+
+        /*
+          $response = "";
+
+          $args = array(
+          array('685000','1000',array('1','1','1'),'post_add','1000'),
+          array('664047','1000',array('1','1','1'),'post_add','1000'),
+          array('630078','1000',array('1','1','1'),'post_add','1000'),
+          array('121500','1000',array('1','1','1'),'post_add','1000'),
+          array('194156','1000',array('1','1','1'),'post_add','1000'),
+          array('678400','1000',array('1','1','1'),'post_add','1000'),
+          );
+
+
+          //        $a = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');
+          //        $a->tarif();
+          //        $response .= $a->getResponseTEST();
+          //
+
+
+          foreach ($args as $arg) {
+          $b = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');
+          $b->tarif($arg[0],$arg[1],$arg[2],$arg[3],$arg[4]);
+          $response .= $b->getResponseTEST($arg[0])."<br/>";
+          unset($b);
+
+          $a = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');
+          $a->tarif($arg[0],$arg[1],$arg[2],$arg[3],$arg[4]*10);
+          $response .= $a->getResponseTEST($arg[0])."<br/>";
+          unset($a);
+
+          $c = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');
+          $c->tarif($arg[0],$arg[1],$arg[2],'post',$arg[4]);
+          $response .= $c->getResponseTEST($arg[0])."<br/>";
+          unset($c);
+
+          $d = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');
+          $d->tarif($arg[0],$arg[1],$arg[2],'post',$arg[4]*10);
+          $response .= $d->getResponseTEST($arg[0])."<br/>";
+          unset($d);
+
+          $e = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');
+          $e->tarif($arg[0],$arg[1],$arg[2],'+post',$arg[4]);
+          $response .= $e->getResponseTEST($arg[0])."<br/>";
+          unset($e);
+
+          $f = new \Biocare\HttpApiBundle\Entity\RuB2CPL('test');
+          $f->tarif($arg[0],$arg[1],$arg[2],'+post',$arg[4]*10);
+          $response .= $f->getResponseTEST($arg[0])."<br/>";
+          unset($f);
+          }
+
+
+
+         */
+
+
         return array('name' => $response);
     }
 
