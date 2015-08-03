9140622248eb79c66c18e7436d74d10081ed9657
Karol Gontarski <karol.gontarski@gmail.com>
2015-08-03 04:32:22 -0400

    Server Push

---
+++ src/Biocare/StorageBundle/Form/CartType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Biocare\StorageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CartType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('createdAt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Biocare\StorageBundle\Entity\Cart'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'biocare_storagebundle_cart';
+    }
+}
