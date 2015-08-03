57d7ec82b7fdca374a122b5955b12cc7e15907e3
Karol Gontarski <karol.gontarski@gmail.com>
2015-08-03 06:15:44 -0400

    Server Push

---
+++ src/Biocare/ProductBundle/Form/CartItemType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Biocare\ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CartItemType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('weight')
+            ->add('value')
+            ->add('currency')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Biocare\ProductBundle\Entity\CartItem'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'biocare_productbundle_cartitem';
+    }
+}
