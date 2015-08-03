7852c89f53826205a3f184c45aaa2ce02c5f3408
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-03 10:59:37 +0200

    Order Panel

--- src/Biocare/ProductBundle/Entity/CartItem.php
+++ src/Biocare/ProductBundle/Entity/CartItem.php
@@ -15,6 +15,22 @@ use Doctrine\ORM\Mapping as ORM;
 class CartItem extends Item{
     
     
+    
+    /**
+     * @ORM/Column(type="value", type="integer")
+     */
+    private $weight;
+    
+    public function getWeight() {
+        return $this->weight;
+    }
+
+    public function setWeight($weight) {
+        $this->weight = $weight;
+        return $this;
+    }
+
+    
     /**
      * @ORM/Column(type="value", type="decimal", precision=15, scale=2)
      */
@@ -28,7 +44,32 @@ class CartItem extends Item{
         $this->value = $value;
         return $this;
     }
+  
+    /**
+     * @ORM\ManyToOne(targetEntity="\Biocare\PriceBundle\Entity\Currency")
+     */
+    private $currency;
+    
+    /**
+     * Set currency
+     *
+     * @param \Biocare\PriceBundle\Entity\Currency $currency
+     * @return Price
+     */
+    public function setCurrency(\Biocare\PriceBundle\Entity\Currency $currency = null) {
+        $this->currency = $currency;
 
+        return $this;
+    }
+
+    /**
+     * Get currency
+     *
+     * @return \Biocare\CampaignBundle\Entity\Currency 
+     */
+    public function getCurrency() {
+        return $this->currency;
+    }
     
     
 }
