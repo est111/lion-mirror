56dfe42c121e333a6c81ea2a35233e7e6c0ebc20
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-03 17:24:42 +0200

    Order Panel

--- src/Biocare/StorageBundle/Controller/StorageController.php
+++ src/Biocare/StorageBundle/Controller/StorageController.php
@@ -34,7 +34,7 @@ class StorageController extends Controller
             $query_1 = $em->createQuery('SELECT p as product FROM BiocareProductBundle:Product p WHERE p.id = :product');
             $query_1->setParameter('product', $product["product"]);
             //$count[$key]["product"] = $query_1->getSingleResult();
-            dump($query_1->getResult());
+            dump($query_1->getResult(0));
         }            
         dump($count);
         return array(
