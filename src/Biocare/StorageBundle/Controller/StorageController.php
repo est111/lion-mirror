b4ef52dedb04b80d7a77884dc3ddedbb2494c5ef
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-03 16:23:25 +0200

    Order Panel

--- src/Biocare/StorageBundle/Controller/StorageController.php
+++ src/Biocare/StorageBundle/Controller/StorageController.php
@@ -27,7 +27,7 @@ class StorageController extends Controller
     public function expoAction($id)
     {
         $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery('SELECT p.id as product, COUNT(*) as qty FROM BiocareProductBundle:Item i JOIN BiocareProductBundle:Product p ON p.id = i.product_id HAVING i.storage_id = :storage GROUP BY p.id');
+        $query = $em->createQuery('SELECT p.id as product, COUNT(p.id) as qty FROM BiocareProductBundle:Item i JOIN  BiocareProductBundle:Product p ON p.id = i.product_id HAVING i.storage_id = :storage GROUP BY p.id');
         $query->setParameter('storage', $id);
         $count = $query->getResult();
         return array(
