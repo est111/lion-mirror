4c4f976d2fbf2bc71d6a23456975525cb6f33733
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-03 12:03:32 +0200

    Order Panel

--- src/Biocare/ProductBundle/Entity/CartItem.php
+++ src/Biocare/ProductBundle/Entity/CartItem.php
@@ -17,6 +17,26 @@ class CartItem extends Item{
     public function __construct() {
         parent::__construct();
     }
+    
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
     /**
      * @ORM\Column(name="weight", type="integer")
      */
