96b25404440cfdfdb262dba831ec3dac97781174
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-04 16:29:36 +0200

    Order Panel

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('82.139.24.15','148.251.29.180','46.77.124.127','84.10.45.26','89.67.220.155','89.67.186.37','127.0.0.1', 'fe80::1', '::1','89.64.134.14')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('178.235.139.118','82.139.24.15','148.251.29.180','46.77.124.127','84.10.45.26','89.67.220.155','89.67.186.37','127.0.0.1', 'fe80::1', '::1','89.64.134.14')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information. '.$_SERVER['REMOTE_ADDR']);
