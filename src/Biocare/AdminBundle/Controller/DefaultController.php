21a9255ed362366156b79117909cea26d52d9aaf
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-04 11:02:46 +0200

    Order Panel

--- src/Biocare/AdminBundle/Controller/DefaultController.php
+++ src/Biocare/AdminBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/admin")
+     * @Route("/admin", name="admin")
      * @Template()
      */
     public function indexAction($name = "Admin Panel")
