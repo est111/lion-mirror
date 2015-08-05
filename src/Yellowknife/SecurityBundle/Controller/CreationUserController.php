42173f11bbd56440c89c15d0c2c837f63f7a5ba0
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-05 12:52:48 +0200

    Order Panel

--- src/Yellowknife/SecurityBundle/Controller/CreationUserController.php
+++ src/Yellowknife/SecurityBundle/Controller/CreationUserController.php
@@ -2,6 +2,7 @@
 
 namespace Yellowknife\SecurityBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
