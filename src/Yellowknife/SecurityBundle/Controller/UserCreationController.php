0c27c12df3d56c99374af3f2973eabe630347b85
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-05 13:47:42 +0200

    Order Panel

--- src/Yellowknife/SecurityBundle/Controller/UserCreationController.php
+++ src/Yellowknife/SecurityBundle/Controller/UserCreationController.php
@@ -43,6 +43,84 @@ class UserCreationController extends Controller {
         if ($request->isMethod('POST')) {
             $form->bind($request);
             $data = $form->getData();
+            $user = new Yellowknife\SecurityBundle\Entity\User();
+            
+            
+            
+            
+            
+            
+                    
+        $user->setEmail($form->get("email")->getData());
+        $email_check = $this->getDoctrine()
+                ->getRepository('YellowknifeUserBundle:User')
+                ->findBy(array(
+            'email' => $user->getEmail()
+        ));
+        
+    if ($email_check) {
+        
+        $this->get('session')->getFlashBag()->add(
+            'warning', 'EMAIL IS ALREADY IN DATABASE FIND IT AND RESET A PASSWORD: ' . $user->getEmail()
+        );
+        return $this->redirect($this->generateUrl('callcenter_show', array('id' => $entity->getCallcenter()->getId())));
+    }
+        
+        $user->setFirstname($form->get("name")->getData());
+        $user->setLastname($form->get("surname")->getData());
+
+        $user_check = $this->getDoctrine()
+                ->getRepository('YellowknifeUserBundle:User')
+                ->findBy(array(
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname()
+        ));
+
+        $username = strtolower($user->getFirstname() . "." . $user->getLastname() . (count($user_check) > 0 ? count($user_check) : ''));
+
+        $user->setUsername($username);
+
+        $generator = new SecureRandom();
+        $password = bin2hex($generator->nextBytes(4));
+
+        $message = \Swift_Message::newInstance()
+                ->setSubject('Password Lion CRM')
+                ->setFrom('karol.gontarski@gmail.com')
+                ->setTo($user->getEmail())
+                ->setBody('User: ' . $username . '  Password: ' . $password, 'text/plain'
+        );
+
+
+        $encoder = $this->container->get('security.password_encoder');
+        $encoded = $encoder->encodePassword($user, $password);
+
+        $user->setPassword($encoded);
+
+        $entity->setUser($user);
+
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+
+            $em->flush();
+
+            $this->container->get('mailer')->send($message);
+            return $this->redirect($this->generateUrl('callcenter_show', array('id' => $entity->getCallcenter()->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+        );
+            
+            
+            
+            
+            
+            
+            
+            
         }
 
 
