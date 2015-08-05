07a484f22d9c39584367e16a463b8c8739d2b939
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-05 14:33:39 +0200

    Order Panel

--- src/Yellowknife/SecurityBundle/Controller/UserCreationController.php
+++ src/Yellowknife/SecurityBundle/Controller/UserCreationController.php
@@ -69,7 +69,7 @@ class UserCreationController extends Controller {
         if ($request->isMethod('POST')) {
             $form->bind($request);
             $data = $form->getData();
-            return $this->redirect($this->generateUrl('admin_user'));
+            return $this->redirect($this->generateUrl('admin_user_new'));
         }
     }
 
