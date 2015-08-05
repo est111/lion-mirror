72d982a8924a86ddc49d994dab0fbb6b968d86f7
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-05 13:39:50 +0200

    Order Panel

--- src/Yellowknife/SecurityBundle/Controller/UserCreationController.php
+++ src/Yellowknife/SecurityBundle/Controller/UserCreationController.php
@@ -24,16 +24,20 @@ class UserCreationController extends Controller {
     public function userCreationAction(Request $request) {
         $data = array();
         $form = $this->createFormBuilder($data)
-                ->add('name', 'text')
-                ->add('surname', 'text')
-                ->add('email', 'email')
-                ->add('locale', 'choice', array('choices' => array(
+                ->add('name', 'text', array('attr' => array('placeholder' => 'Your name'), 'label' => false))
+                ->add('surname', 'text', array('attr' => array('placeholder' => 'Your name'), 'label' => false))
+                ->add('email', 'email', array('attr' => array('placeholder' => 'Your name'), 'label' => false))
+                ->add('locale', 'choice', array(
+                    'choices' => array(
                         'en' => 'English',
                         'ru' => 'Russian',
                         'cz' => 'Czech',
                         'sk' => 'Slovak',
                         'hu' => 'Hungarian',
-            )))
+                    ),
+                    'attr' => array(
+                        'placeholder' => 'Your name'
+                    ), 'label' => false))
                 ->getForm();
 
         if ($request->isMethod('POST')) {
