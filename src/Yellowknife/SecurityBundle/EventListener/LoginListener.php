17ea5c7220c1c9b8b1f91dd27614f6bc0387d91b
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-05 10:25:43 +0200

    Order Panel

--- src/Yellowknife/SecurityBundle/EventListener/LoginListener.php
+++ src/Yellowknife/SecurityBundle/EventListener/LoginListener.php
@@ -1,89 +1,43 @@
 <?php
-
 namespace Yellowknife\SecurityBundle\EventListener;
 
-use Symfony\Component\Security\Core\Event\AuthenticationEvent;
-use Symfony\Component\Security\Core\SecurityContext;
+
+
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine;
-
-/**
- * Description of LoginListener
- *
- * @author Karol Gontarski
- */
-class LoginListener {
-
-    /** @var \Symfony\Component\Security\Core\SecurityContext */
-    private $securityContext;
-
-    /** @var \Doctrine\ORM\EntityManager */
-    private $em;
-
-    private $locale;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+ 
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    protected 
+        $router,
+        $security;
     
-    private $user;
-    
-    private $router;
-
-
-    /**
-     * Constructor
-     * 
-     * @param SecurityContext $securityContext
-     * @param Doctrine        $doctrine
-     */
-    public function __construct(SecurityContext $securityContext, Doctrine $doctrine, Router $router) {
-        $this->securityContext = $securityContext;
-        $this->em = $doctrine->getEntityManager();
+    public function __construct(Router $router, SecurityContext $security)
+    {
         $this->router = $router;
-                
+        $this->security = $security;
     }
-
-    /**
-     * Do the magic.
-     * 
-     * @param InteractiveLoginEvent $event
-     */
-    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event) {
+    
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        // URL for redirect the user to where they were before the login process begun if you want.
+        // $referer_url = $request->headers->get('referer');
         
-        $this->user = $event->getAuthenticationToken()->getUser();
-        //$this->locale = $this->user->getLocale();
-        
+        // Default target for unknown roles. Everyone else go there.
         $url = 'panel';
-        
-        if ($this->securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
-            // user has just logged in
-        }
-
-        if ($this->securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            // user has logged in using remember_me cookie
-        }
-        
-        
-        
-        if ($this->securityContext->isGranted('ROLE_ADMIN')) {
-            $url = 'admin';
-        }
-        if ($this->securityContext->isGranted('ROLE_USER')) {
+        if($this->security->isGranted('ROLE_USER')) {
             $url = 'panel';
         }
-        
+        elseif($this->security->isGranted('ROLE_ADMIN')) {
+            $url = 'admin';
+        }
         $response = new RedirectResponse($this->router->generate($url));
             
         return $response;
-        // ...
     }
-
-    public function onKernelResponse(FilterResponseEvent $event) {
-
-        if (null !== $this->locale) {
-            $request = $event->getRequest();
-            //$request->getSession()->set('_locale', $this->locale);
-        }
-    }
-
 }
