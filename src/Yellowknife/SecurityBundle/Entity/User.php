093a4bc9e4a311f2f37ea4a385186b1f76b0d830
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-07 16:44:03 +0200

    Order Panel

--- src/Yellowknife/SecurityBundle/Entity/User.php
+++ src/Yellowknife/SecurityBundle/Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, \Serializable
     private $password;    
     
     /**
-     * @ORM\Column(type="datetimez")
+     * @ORM\Column(type="datetime")
      */
     private $passwordChangedAt;
 
