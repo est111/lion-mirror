d1f80a3246925c55ee7b77e22311b607dc14b077
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-07 19:07:06 +0200

    Order Panel

--- src/Yellowknife/SecurityBundle/Authentication/Handler/LogoutSuccessHandler.php
+++ src/Yellowknife/SecurityBundle/Authentication/Handler/LogoutSuccessHandler.php
@@ -3,14 +3,18 @@ namespace Yellowknife\SecurityBundle\Authentication\Handler;
 
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Routing\Router;
 
 class LogoutSuccessHandler implements LogoutSuccessHandlerInterface {
 
   private $security;  
+  private $router;  
 
-  public function __construct(SecurityContext $security) {
-    $this->security = $security;
-  }
+    public function __construct(Router $router, SecurityContext $security)
+    {
+        $this->router = $router;
+        $this->security = $security;
+    }
 
   public function onLogoutSuccess(Request $request) {
      $user = $this->security->getToken()->getUser();
