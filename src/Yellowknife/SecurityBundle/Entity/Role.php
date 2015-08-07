3ef7246cf7aa27e250a9dd8ec6c447bddc4f7e55
Karol Gontarski <Karol Gontarski@karol-gontarski>
2015-08-07 15:51:52 +0200

    Order Panel

---
+++ src/Yellowknife/SecurityBundle/Entity/Role.php
@@ -0,0 +1,123 @@
+<?php
+// src/Yellowknife/SecurityBundle/Entity/Role.php
+namespace Yellowknife\SecurityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+
+/**
+ * Role class
+ *
+ * @ORM\Entity()
+ */
+class Role implements RoleInterface
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="role", type="string", length=255)
+     */
+    private $role;
+	
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+	
+    /**
+     * @ORM\ManyToMany(targetEntity="User", mappedBy="userRoles")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+	
+    /**
+     * Set role
+     *
+     * @param string $Role
+     * @return Role
+     */
+    public function setRole($role)
+    {
+		
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string 
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+    
+
+    /**
+     * Get users
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+	
+	 	
+    public function __toString()
+    {
+            return $this->getName();
+    }
+        
+     
+}
